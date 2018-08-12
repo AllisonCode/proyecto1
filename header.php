5e9d35fc1dd7ec5434b70b89e60ef01a15dcec69
Chris Fuentes <fuenteschris75@gmail.com>
2018-08-11 22:06:49 -0600

    Actualizacion

--- header.php
+++ header.php
@@ -43,7 +43,7 @@
                 <div class="container-fluid">
                     <div class="header-mobile-inner">
                         <a class="logo" href="index.html">
-                            <img src="images/icon/logo.png" alt="CoolAdmin" />
+                            <img src="images/icon/logo-movil.png" alt="AutoSAL" />
                         </a>
                         <button class="hamburger hamburger--slider" type="button">
                             <span class="hamburger-box">
@@ -124,7 +124,7 @@
         <aside class="menu-sidebar d-none d-lg-block">
             <div class="logo">
                 <a href="#">
-                    <img src="images/icon/logo.png" alt="AllisonCode" />
+                    <img src="images/icon/logo.png" alt="AutoSAL" />
                 </a>
             </div>
             <div class="menu-sidebar__content js-scrollbar1">
