9dace5ba67c8e76bbba969983176c932232711f8
Chris Fuentes <fuenteschris75@gmail.com>
2018-08-12 03:39:30 -0600

    Ultima actualizacion 11-08-2018

--- usuarios.php
+++ usuarios.php
@@ -7,41 +7,62 @@
 
 
 <div id="editar" style="display: none;"></div>
-
 <div class="card">
-                                    <div class="card-header">Actualizar Usuario</div>
+                                    <div class="card-header">Editar Usuario</div>
                                     <div class="card-body card-block">
-
+                                       
                                             <div class="form-group">
                                                 <div class="input-group">
+                                                    <div class="input-group-addon">Nombre(s)</div>
+                                                    <input type="text" id="nombre" name="nombre" class="form-control" value="Juancito">
                                                     <div class="input-group-addon">
                                                         <i class="fa fa-user"></i>
                                                     </div>
-                                                    Nombre(s):<input type="text" id="nombre" name="nombre" placeholder="Nombre(s)" class="form-control" value="Juancito">
                                                 </div>
                                             </div>
-                                            <div class="form-group">
+											<div class="form-group">
                                                 <div class="input-group">
+                                                    <div class="input-group-addon">Apellido(s)</div>
+                                                    <input type="text" id="apellido" name="apellido" class="form-control" value="Pérez">
                                                     <div class="input-group-addon">
-                                                        <i class="fa fa-envelope"></i>
+                                                        <i class="fa fa-user"></i>
                                                     </div>
-                                                    <input type="email" id="email" name="email" placeholder="Email" class="form-control">
                                                 </div>
                                             </div>
-                                            <div class="form-group">
+											<div class="form-group">
                                                 <div class="input-group">
+                                                    <div class="input-group-addon">Usuario</div>
+                                                    <input type="text" id="username" name="username" class="form-control" value="Juancito">
+                                                    <div class="input-group-addon">
+                                                        <i class="fa fa-user"></i>
+                                                    </div>
+                                                </div>
+                                            </div>
+											<div class="form-group">
+                                                <div class="input-group">
+                                                    <div class="input-group-addon">Contraseña</div>
+                                                    <input type="password" id="password" name="password" class="form-control">
                                                     <div class="input-group-addon">
                                                         <i class="fa fa-asterisk"></i>
                                                     </div>
-                                                    <input type="password" id="password" name="password" placeholder="Password" class="form-control">
                                                 </div>
                                             </div>
-                                            <div class="form-actions form-group">
-                                                <button type="submit" class="btn btn-success btn-sm">Submit</button>
+                                            <div class="form-group">
+                                                <div class="input-group">
+                                                    <div class="input-group-addon">Email</div>
+                                                    <input type="email" id="email" name="email" class="form-control">
+                                                    <div class="input-group-addon">
+                                                        <i class="fa fa-envelope"></i>
+                                                    </div>
+                                                </div>
                                             </div>
+                                            
+                                            <div class="form-actions form-group">
+                                                <button type="submit" class="btn btn-primary btn-sm">Submit</button>
+                                            </div>
+                                        
                                     </div>
                                 </div>
-
 
 <body onload="listartodos()">
  <div id="listar">
