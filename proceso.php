6905c229cd4386ab17840961120aeafa09418be9
Chris Fuentes <fuenteschris75@gmail.com>
2018-08-19 22:27:42 -0600

    Actualizacion

--- proceso.php
+++ proceso.php
@@ -4,13 +4,15 @@ $usuario=$_POST['username'];
 $contrasena=$_POST['password'];
 
 $contra = md5 ($contrasena);
-include("conexion.php");
+include("ajax/database.php");
 
-$proceso = $conexion->query("SELECT * FROM usuarios WHERE username='$usuario' AND password='$contra' ");
+$proceso = $conn->query("SELECT * FROM usuarios WHERE username='$usuario' AND password='$contra' ");
     
     if($resultado = mysqli_fetch_array($proceso)){
         $_SESSION['u_usuario'] = $usuario;
-        //$_SESSION['u_correo']
+        $_SESSION['u_correo'] = $resultado['email'];
+		$_SESSION['u_nombre'] = $resultado['nombre'].' '.$resultado['apellido'];
+		$_SESSION['u_rol'] = $resultado['rol'];
        header("Location:inicio");
       
     }
--- ventas.php
+++ ventas.php
@@ -1,14 +1,14 @@
 <?php include 'header.php';?>
 
-                <div class="section__content section__content--p30">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="col-lg-9">
-                                <div class="table-responsive table--no-card m-b-30">
-                                    <table class="table table-borderless table-striped table-earning">
-<?php include 'header.php';
-include("conexion.php");
-$consulta = consulta();
+              
+<?php
+include('ajax/database.php');
+$sql = 'SELECT idVenta, nombreZona, nombreModelo, nombre, apellido, precio, fechahora, cliente, nombreMetodo from ventas
+        join zonas on ventas.idZona = zonas.idZona
+        join modelos on ventas.idModelo = modelos.idModelo 
+        join usuarios on ventas.idUsuario = usuarios.idUsuario 
+        join metodopago on ventas.metodopago = metodopago.idMetodo';
+$result = $conn->query($sql);
 ?>
    
 
@@ -41,7 +41,7 @@ $consulta = consulta();
                                          
                                         <tbody>
                                             <?php
-                                            while($venta = $consulta->fetch_assoc())
+                                            while($venta = $result->fetch_assoc())
                                             {
                                                 ?>
                                             <tr>
@@ -65,56 +65,7 @@ $consulta = consulta();
                                
                              </div>
                             </div>
-<<<<<<< HEAD
-                            <div class="col-lg-3">
-                                <div class="au-card au-card--bg-blue au-card-top-countries m-b-30">
-                                    <div class="au-card-inner">
-                                        <div class="table-responsive">
-                                            <table class="table table-top-countries">
-                                                <tbody>
-                                                    <tr>
-                                                        <td>United States</td>
-                                                        <td class="text-right">$119,366.96</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Australia</td>
-                                                        <td class="text-right">$70,261.65</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>United Kingdom</td>
-                                                        <td class="text-right">$46,399.22</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Turkey</td>
-                                                        <td class="text-right">$35,364.90</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Germany</td>
-                                                        <td class="text-right">$20,366.96</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>France</td>
-                                                        <td class="text-right">$10,366.96</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Australia</td>
-                                                        <td class="text-right">$5,366.96</td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>Italy</td>
-                                                        <td class="text-right">$1639.32</td>
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-6">
 
-=======
 <script src="js/xlsx.core.min.js"></script>
 <script src="js/Blob.min.js"></script>
 <script src="js/FileSaver.min.js"></script>
